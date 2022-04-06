586f0a3a094035ff7e91f6f28572f54c872de3eb
higashiyama-k105r <higashiyama-k105r@tsuushin1.onmicrosoft.com>
2022-04-06 11:49:35 +0900

    bar add login menu
    next tab

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="blue-grey lighten-4"
+      light
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          :src="require('../src/assets/rise_logo.png')"
+          transition="scale-transition"
+          width="150"
+        />
+        <v-btn href="../" plain>
+          <h1>信頼性ＨＰ</h1>
+        </v-btn>
+      </div>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-account</v-icon>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            plain
+            v-bind="attrs"
+            v-on="on"
+          >
+            root ▼
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
+
   components: {
     HelloWorld
-  }
+  },
+  data: () => ({
+    items: [
+      { title: 'ログアウト' },
+      { title: 'パスワード変更' },
+      { title: 'ユーザ管理' }
+    ]
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
